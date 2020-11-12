33b2416a86feb4fa45b9b7ab0373198293683ab6
goodnews <goodnewsj62@gmail.com>
2020-11-12 13:38:34 +0100

    second commit stable first version

--- flask_app/home/blog.py
+++ flask_app/home/blog.py
@@ -1,5 +1,5 @@
-from flask import Blueprint,request,redirect,render_template,g,abort,url_for
-from flask_login import current_user,login_required
+from flask import Blueprint, request, redirect, render_template, g, abort, url_for
+from flask_login import current_user, login_required
 from functools import wraps
 from recommender import get_recommendations
 from top import recommend_top
@@ -8,51 +8,47 @@ from random import randrange
 from flask_app.model import Movie
 
 
-
-
-
-blog = Blueprint('blog',__name__)
+blog = Blueprint('blog', __name__)
 
 
 def anonymous(func):
     @wraps(func)
-    def anonymous_wrapper(*args,**kwargs):
+    def anonymous_wrapper(*args, **kwargs):
         if not current_user.is_authenticated:
-            return func(*args,**kwargs)
+            return func(*args, **kwargs)
         return redirect(url_for('blog.recommend_home'))
     return anonymous_wrapper
+
 
 @blog.route("/home")
 @anonymous
 def home_page():
-    return  render_template('blog/index.html')
+    return render_template('blog/index.html')
 
 
-@blog.route("/home/recommend", methods= ["GET","POST"])
+@blog.route("/home/recommend", methods=["GET", "POST"])
 def recommend_home():
-    # movie = Movie.query.all()
-
-    # user = User.query.filter_by(username = current_user)
-    recommended = None
-    # if current_user.is_authenticated:
-    #     if len(user.movie) > 10:
-    #         movie_id = user.movie
 
     context = dict()
     top_rated = recommend_top().values.tolist()
     length = len(top_rated)
-    context['top rated'] = [Movie.query.filter_by(title=top_rated[n]).first() for n in range(length)][:10]
-    context['recent'] = Movie.query.filter(Movie.release_date.contains('2016')).all()[:10]
-    context['discover/explore movies'] = [Movie.query.filter_by(id=randrange(1, 3000)).first() for each in range(10)]
+    context['top rated'] = [Movie.query.filter_by(
+        title=top_rated[n]).first() for n in range(length)][:10]
+    context['recent'] = Movie.query.filter(
+        Movie.release_date.contains('2016')).all()[:10]
+    context['discover/explore movies'] = [Movie.query.filter_by(
+        id=randrange(1, 3000)).first() for each in range(10)]
 
+    if current_user.is_authenticated:
+        context['recommended'] = Movie.query.filter(Movie.genres.contains(current_user.genre_a.lower(
+        ) or current_user.genre_b.lower() or current_user.genre_c.lower() or current_user.genre_d.lower())).all()[:10]
 
     form = Search()
     result = None
     response = True
     keyword = ''
 
-
-    if  form.validate_on_submit():
+    if form.validate_on_submit():
         keyword = form.search.data
         try:
             result_final = get_recommendations(keyword)
@@ -60,33 +56,19 @@ def recommend_home():
             response = False
         names = []
 
-
         for i in range(len(result_final)):
             names.append(result_final.iloc[i][0])
 
-
-        result = [Movie.query.filter_by(title = i).first() for i in names]
-        # ratings = [trunc(float(n)/1.5) for n in rating]
-        # for each in genre:
-        #     each = each.translate(each.maketrans("", "", '""[],'))
-        #     each = each.translate(each.maketrans("", "", "'"))
-        #     first, *args, third = each.split()
-        #
-        #     if len(args) > 0:
-        #         genres.append((first, args[0], third))
-        #     else:
-        #         genres.append((first, third))
+        result = [Movie.query.filter_by(title=i).first() for i in names]
+    return render_template('blog/home.html', response=response, context=context, keyword=keyword, results=result, form=form)
 
 
-    return render_template('blog/home.html',response = response, context = context
-                           ,keyword = keyword, results = result,form = form)
-
-@blog.route('/details/<int:movie_id>',methods = ['GET'])
+@blog.route('/details/<int:movie_id>', methods=['GET'])
 def details(movie_id):
 
     movie = Movie.query.get(movie_id)
     if movie == None:
-        return  abort(404)
+        return abort(404)
 
     result = []
     if movie:
@@ -95,48 +77,54 @@ def details(movie_id):
         for i in range(len(result_final)):
             names.append(result_final.iloc[i][0])
 
-        result = [Movie.query.filter_by(title=i).first() for i in names]
+        result = [Movie.query.filter_by(title=i).first() for i in names][1:]
 
-    return render_template('blog/movie_details.html', result = result, movie = movie)
+    return render_template('blog/movie_details.html', result=result, movie=movie)
 
-@blog.route('/home/movies/top_rated', methods = ['GET'])
+
+@blog.route('/home/movies/top_rated', methods=['GET'])
 def top():
     top_rated = recommend_top().values.tolist()
     length = len(top_rated)
-    movie = [Movie.query.filter_by(title=top_rated[n]).first() for n in range(length)]
+    movie = [Movie.query.filter_by(title=top_rated[n]).first()
+             for n in range(length)]
 
-    return  render_template('blog/all.html', movie = movie)
+    return render_template('blog/all.html', movie=movie, title="top rated movies")
 
-@blog.route('/home/movies/recent', methods = ['GET'])
+
+@blog.route('/home/movies/recent', methods=['GET'])
 def recent():
     movie = Movie.query.filter(Movie.release_date.contains('2016')).all()
-    return render_template('blog/all.html', movie = movie)
+    return render_template('blog/all.html', movie=movie, title="recent movie")
 
-@blog.route('/home/movies/discover', methods = ['GET'])
+
+@blog.route('/home/movies/discover', methods=['GET'])
 def discover():
-    movie = [Movie.query.filter_by(id=randrange(1, 3000)).first() for each in range(200)]
-    return  render_template('blog/all.html', movie = movie)
+    movie = [Movie.query.filter_by(id=randrange(
+        1, 3000)).first() for each in range(200)]
+    return render_template('blog/all.html', movie=movie, title="discover/explore movie")
 
 
-# @blog.route('/home/movie/recommended', methods = ['GET'])
-# def recommended():
-#     pass
-
+@blog.route('/home/movie/recommended', methods=['GET'])
+@login_required
+def recommended():
+    movie = Movie.query.filter(Movie.genres.contains(current_user.genre_a.lower(
+    ) or current_user.genre_b.lower() or current_user.genre_c.lower() or current_user.genre_d.lower())).all()
+    return render_template('blog/all.html', movie=movie, title="recommended")
 
 
 # @blog.route('/home/movie/rate', methods = ['GET'])
 # def rate():
 #     pass
 
-@blog.route('/movie/genres/<string:genre>', methods = ['GET'])
+@blog.route('/movie/genres/<string:genre>', methods=['GET'])
 def movie_genre(genre):
     movie = Movie.query.filter(Movie.genres.contains(genre)).all()
     if len(movie) == 0:
         return abort(404)
-    return render_template('blog/all.html', movie = movie)
+    return render_template('blog/all.html', name=genre, movie=movie, title='genre')
 
 
-@blog.route('/movie/genres', methods =['GET'])
+@blog.route('/movie/genres', methods=['GET'])
 def movie_genres():
     return render_template('blog/genres.html')
-
