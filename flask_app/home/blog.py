764a03ec16685bdd2b9239708674caff9f73ef21
goodnews <goodnewsj62@gmail.com>
2020-11-14 15:40:24 +0100

    third commit pagination

--- flask_app/home/blog.py
+++ flask_app/home/blog.py
@@ -6,7 +6,8 @@ from top import recommend_top
 from .form import Search
 from random import randrange
 from flask_app.model import Movie
-
+from config.configuration import Config
+from flask_sqlalchemy import Pagination
 
 blog = Blueprint('blog', __name__)
 
@@ -86,31 +87,89 @@ def details(movie_id):
 def top():
     top_rated = recommend_top().values.tolist()
     length = len(top_rated)
+
+    try:
+        page = request.args.get('page', 1, type=int)
+    except ValueError:
+        page = 1
+
     movie = [Movie.query.filter_by(title=top_rated[n]).first()
              for n in range(length)]
 
-    return render_template('blog/all.html', movie=movie, title="top rated movies")
+    item = (page - 1) * Config.ITEMS_PER_PAGE
+    item = movie[item: item + 20]
+
+    pagination = Pagination(Movie.query.all(
+    ), page=page, per_page=Config.ITEMS_PER_PAGE, total=len(movie), items=item)
+
+    next_url = url_for(
+        'blog.top', page=pagination.next_num) if pagination.has_next else None
+
+    prev_url = url_for(
+        'blog.top', page=pagination.prev_num) if pagination.has_prev else None
+
+    return render_template('blog/all.html', movie=pagination.items, prev_url=prev_url, next_url=next_url, title="top rated movies")
 
 
 @blog.route('/home/movies/recent', methods=['GET'])
 def recent():
-    movie = Movie.query.filter(Movie.release_date.contains('2016')).all()
-    return render_template('blog/all.html', movie=movie, title="recent movie")
+    page = request.args.get('page', 1, type=int)
+    movie = Movie.query.filter(Movie.release_date.contains(
+        '2016')).paginate(page, Config.ITEMS_PER_PAGE, True)
+
+    next_url = url_for(
+        'blog.recent', page=movie.next_num) if movie.has_next else None
+
+    prev_url = url_for(
+        'blog.recent', page=movie.prev_num) if movie.has_prev else None
+
+    return render_template('blog/all.html', movie=movie.items, next_url=next_url, prev_url=prev_url, title="recent movie")
 
 
 @blog.route('/home/movies/discover', methods=['GET'])
 def discover():
+    try:
+        page = request.args.get('page', 1, type=int)
+    except ValueError:
+        page = 1
+
     movie = [Movie.query.filter_by(id=randrange(
         1, 3000)).first() for each in range(200)]
-    return render_template('blog/all.html', movie=movie, title="discover/explore movie")
+
+    # get the start index base on page number
+    item = (page - 1)*(Config.ITEMS_PER_PAGE)
+    end_index = item + (Config.ITEMS_PER_PAGE)
+
+    # for page 1 [0:20] page2[20:41]
+    item = movie[item: end_index]
+
+    pagination = Pagination(Movie.query.all(), page,
+                            Config.ITEMS_PER_PAGE, len(movie), item)
+
+    prev_url = url_for(
+        'blog.discover', page=pagination.prev_num) if pagination.has_prev else None
+
+    next_url = url_for(
+        'blog.discover', page=pagination.next_num) if pagination.has_next else None
+
+    return render_template('blog/all.html', movie=pagination.items, next_url=next_url, prev_url=prev_url, title="discover/explore movie")
 
 
 @blog.route('/home/movie/recommended', methods=['GET'])
 @login_required
 def recommended():
+    page = request.args.get('page', 1, type=int)
+
     movie = Movie.query.filter(Movie.genres.contains(current_user.genre_a.lower(
-    ) or current_user.genre_b.lower() or current_user.genre_c.lower() or current_user.genre_d.lower())).all()
-    return render_template('blog/all.html', movie=movie, title="recommended")
+    ) or current_user.genre_b.lower() or current_user.genre_c.lower() or current_user.genre_d.lower())).paginate(page, Config.ITEMS_PER_PAGE, True)
+
+    prev_url = url_for('blog.recommended',
+                       page=movie.prev_num) if movie.has_prev else None
+
+    next_url = url_for('blog.recommended',
+                       page=movie.next_num) if movie.has_next else None
+
+    return render_template('blog/all.html', movie=movie.items, prev_url=prev_url, next_url=next_url, title="recommended")
 
 
 # @blog.route('/home/movie/rate', methods = ['GET'])
@@ -119,10 +178,23 @@ def recommended():
 
 @blog.route('/movie/genres/<string:genre>', methods=['GET'])
 def movie_genre(genre):
-    movie = Movie.query.filter(Movie.genres.contains(genre)).all()
+    page = request.args.get('page', 1, type=int)
+
+    movie = Movie.query.filter(Movie.genres.contains(
+        genre)).all()
+
     if len(movie) == 0:
         return abort(404)
-    return render_template('blog/all.html', name=genre, movie=movie, title='genre')
+    movie = Movie.query.filter(Movie.genres.contains(
+        genre)).paginate(page, Config.ITEMS_PER_PAGE, True)
+
+    prev_url = url_for('blog.movie_genre',
+                       genre=genre, page=movie.prev_num) if movie.has_prev else None
+
+    next_url = url_for('blog.movie_genre',
+                       genre=genre, page=movie.next_num) if movie.has_next else None
+
+    return render_template('blog/all.html', name=genre, movie=movie.items, prev_url=prev_url, next_url=next_url, title='genre')
 
 
 @blog.route('/movie/genres', methods=['GET'])
